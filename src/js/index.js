95141bb9c55b50e57a967328e9d76133af538074
Seoyeon Ko <55572222+seoyeonko@users.noreply.github.com>
2022-06-05 17:35:16 +0900

    10. 메뉴 추가하기 - 클릭 버튼을 눌렀을 때 메뉴 추가하기 | 클릭 이벤트 핸들링

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
 // - [x] 메뉴 이름 입력 -> enter 키 입력 추가
+// - [x] 메뉴 이름 입력 -> 확인 버튼 클릭 입력 추가
 // - [x] 추가되는 메누 마크업은 `ul#espresso-menu-list` 안에 삽입
 // - [x] 총 메뉴 개수 count해 상단에 보이기
 // - [x] 메뉴 추가시; input 빈 값으로 초기화
-// - [ ] 사용자 입력값이 빈 값; 추가되지 않음
+// - [x] 사용자 입력값이 빈 값; 추가되지 않음
 
 const $ = (selector) => document.querySelector(selector);
 
@@ -14,21 +15,15 @@ function App() {
     e.preventDefault();
   });
 
-  // 메뉴 입력 받기
-  $('#espresso-menu-name').addEventListener('keypress', (e) => {
-    if (e.key !== 'Enter') {
-      return;
-    }
-
+  const addMenuName = () => {
     if ($('#espresso-menu-name').value === '') {
       alert('값을 입력해주세요');
       return;
     }
 
-    if (e.key === 'Enter') {
-      const espressoMenuName = $('#espresso-menu-name').value;
-      const menuItemTemplate = (espressoMenuName) => {
-        return `<li class="menu-list-item d-flex items-center py-2">
+    const espressoMenuName = $('#espresso-menu-name').value;
+    const menuItemTemplate = (espressoMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
         <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
         <button
           type="button"
@@ -43,20 +38,28 @@ function App() {
           삭제
         </button>
       </li>`;
-      };
+    };
 
-      $('#espresso-menu-list').insertAdjacentHTML(
-        'beforeend',
-        menuItemTemplate(espressoMenuName)
-      );
+    $('#espresso-menu-list').insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplate(espressoMenuName)
+    );
 
-      // li 개수 카운팅
-      const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-      $('.menu-count').innerText = `총 ${menuCount}개`;
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount}개`;
+    $('#espresso-menu-name').value = '';
+  };
 
-      // 메뉴 추가시; input 초기화
-      $('#espresso-menu-name').value = '';
+  $('#espresso-menu-submit-button').addEventListener('click', () => {
+    addMenuName();
+  });
+
+  // 메뉴 입력 받기
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    if (e.key !== 'Enter') {
+      return;
     }
+    addMenuName();
   });
 }
 
