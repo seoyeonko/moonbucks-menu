0033261ff46367ee1e112cb3f0caacf943ab53c4
Seoyeon Ko <55572222+seoyeonko@users.noreply.github.com>
2022-06-05 18:43:53 +0900

    14. 스텝1 리팩터링

--- src/js/index.js
+++ src/js/index.js
@@ -24,28 +24,6 @@ function App() {
     $('.menu-count').innerText = `총 ${menuCount}개`;
   };
 
-  $('#espresso-menu-list').addEventListener('click', (e) => {
-    if (e.target.classList.contains('menu-edit-button')) {
-      const $menuName = e.target.closest('li').querySelector('.menu-name');
-      const updatedMenuName = prompt(
-        '메뉴명을 수정하세요',
-        $menuName.innerText
-      );
-      $menuName.innerText = updatedMenuName;
-    }
-
-    if (e.target.classList.contains('menu-remove-button')) {
-      if (confirm('정말 삭제할까요?')) {
-        e.target.closest('li').remove();
-      }
-      updateMenuCount();
-    }
-  });
-
-  $('#espresso-menu-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
-
   const addMenuName = () => {
     if ($('#espresso-menu-name').value === '') {
       alert('값을 입력해주세요');
@@ -79,9 +57,34 @@ function App() {
     $('#espresso-menu-name').value = '';
   };
 
-  $('#espresso-menu-submit-button').addEventListener('click', () => {
-    addMenuName();
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+    const updatedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText);
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuName = (e) => {
+    if (confirm('정말 삭제할까요?')) {
+      e.target.closest('li').remove();
+    }
+    updateMenuCount();
+  };
+
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      updateMenuName(e);
+    }
+
+    if (e.target.classList.contains('menu-remove-button')) {
+      removeMenuName(e);
+    }
   });
+
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+
+  $('#espresso-menu-submit-button').addEventListener('click', addMenuName);
 
   $('#espresso-menu-name').addEventListener('keypress', (e) => {
     if (e.key !== 'Enter') {
